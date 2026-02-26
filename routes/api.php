7d5eb80a12d3b5f17a2cbb0042ad99df07854b8c
lucas <lucasdominguesofficial@gmail.com>
2026-02-26 14:03:53 -0300

    teste logtelegram

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/test', function (Request $request) {
+
+    Log::channel('telegram')->critical('log telegram Base api test');
     return Log::info('teste de log');
 });
 Route::fallback(fn() => response(["message" => 'Página não encontrada'], 404));
